e1bcd2b5c33b17adddf38caf08bd52338d32d55a
bibimoni <proquadi66@gmail.com>
2025-11-30 21:52:36 +0700

    feat: added graceful shutdown

--- src/Services/submission-judge/src/cmd/main.go
+++ src/Services/submission-judge/src/cmd/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	appctx "github.com/bibimoni/Online-judge/submission-judge/src/components"
 	"github.com/bibimoni/Online-judge/submission-judge/src/infrastructure/config"
@@ -62,7 +66,22 @@ func main() {
 		WriteTimeout: cfg.Server.WriteTimeout,
 	}
 
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatal().Err(err).Msgf("Failed to start Submission-Judge server: %s", err)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msgf("Failed to start Submission-Judge server: %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("Server forced to shutdown")
 	}
+
+	log.Info().Msg("Server exiting")
 }
